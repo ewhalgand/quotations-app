8041072c4c47bdf0e8235f4771c5ad61896dc231
ewhalgand <halgandewen44@gmail.com>
2025-02-28 12:30:11 +0100

    feat: finish

--- script.js
+++ script.js
@@ -3,27 +3,39 @@ const text = document.querySelector('#enterTheQuote');
 const author = document.querySelector('#author');
 const quoteList = document.querySelector('#quote-list')
 
+
 let quoteCount = 0;
 
 formQuote.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     const textValue = text.value;
     const authorValue = author.value
+    
+    const quoteData = {
+        author: textValue,
+        text: authorValue,
+    }
 
-    addQuote(textValue, authorValue)
+    // window.localStorage.setItem("text", textValue)
+    // window.localStorage.setItem("author", authorValue)
+    localStorage.setItem("quoteData", JSON.stringify(quoteData));
+
+    addQuote()
 })
 
 const addQuote = (quote, author) => {
     const count = document.getElementById('count')
 
+   const data = JSON.parse(localStorage.getItem('quoteData'))
+
     const quoteElement = document.createElement("p")
     quoteElement.classList.add("text")
-    quoteElement.innerText = `Citation: ${quote}`
+    quoteElement.innerText = `Citation: ${data.text}`
 
     const authorElement = document.createElement("p")
     authorElement.classList.add("author")
-    authorElement.innerText = `Auteur(rice): ${author}`
+    authorElement.innerText = `Auteur(rice): ${data.author}`
 
     const quoteContainerElement = document.createElement("div")
     quoteContainerElement.classList.add("quote")
