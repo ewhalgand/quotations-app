b03493344f646b640d167f58a3f41f932a01b362
ewhalgand <halgandewen44@gmail.com>
2025-02-28 11:00:26 +0100

    feat: form functional

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const formQuote = document.querySelector('#formQuote');
-const text = document.getElementById('enterTheQuote');
+const text = document.querySelector('#enterTheQuote');
 const author = document.querySelector('#author');
+const quoteList = document.querySelector('#quote-list')
 
 formQuote.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -9,3 +10,20 @@ formQuote.addEventListener('submit', (e) => {
     const authorValue = author.value
     console.log(textValue, authorValue)
 })
+
+function addQuote(quote, author) {
+    const quoteElement = document.createElement("p")
+    quoteElement.classList.add("text")
+    quoteElement.innerText = `Citation: ${quote}`
+
+    const authorElement = document.createElement("p")
+    authorElement.classList.add("author")
+    authorElement.innerText = `Auteur(rice): ${author}`
+
+    const quoteContainerElement = document.createElement("div")
+    quoteContainerElement.classList.add("quote")
+    quoteContainerElement.appendChild(quoteElement)
+    quoteContainerElement.appendChild(authorElement)
+
+    quoteList.appendChild(quoteContainerElement)
+}
